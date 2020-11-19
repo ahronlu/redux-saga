cccdeaacfab3b343be3e0ba8741794f5e034c9ed
koftov <ahronlu@gmail.com>
2020-11-19 15:38:42 +0200

    2nd commit

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 export const fetchData = async () => {
   try {
-    const response = await fetch(
-      "https://cors-anywhere.herokuapp.com/https://randomuser.me/api/?results=28"
-    );
+    const response = await fetch("https://randomuser.me/api/?results=28");
     const data = await response.json();
     return data;
   } catch (e) {
